3e5e708bdbf17e35a7c521410f9b61874081a316
jacksonbates <jacksonbates@hotmail.com>
2022-01-05 23:08:56 +1100

    Updates

---
+++ precache-manifest.7d8482b008e5779e730c69aeffbff91f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06cd93383ac757c24d382b070f982377",
+    "url": "/skate-setup-calculator/index.html"
+  },
+  {
+    "revision": "0cb048789005b11aaaac",
+    "url": "/skate-setup-calculator/static/css/2.291b88e4.chunk.css"
+  },
+  {
+    "revision": "df17c9e418660fbf2c63",
+    "url": "/skate-setup-calculator/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "0cb048789005b11aaaac",
+    "url": "/skate-setup-calculator/static/js/2.57485a89.chunk.js"
+  },
+  {
+    "revision": "37fac929e9ec5b38bfd83c5fc6405d00",
+    "url": "/skate-setup-calculator/static/js/2.57485a89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df17c9e418660fbf2c63",
+    "url": "/skate-setup-calculator/static/js/main.766824d5.chunk.js"
+  },
+  {
+    "revision": "9e7fe2762cc1e979b0a2",
+    "url": "/skate-setup-calculator/static/js/runtime-main.0bbc664a.js"
+  }
+]);
